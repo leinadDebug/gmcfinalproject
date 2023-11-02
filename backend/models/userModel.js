f2dd40d4e40edf2f2bad164f2dc658ece8a1ead5
leinadDebug <akujuaobidaniel@gmail.com>
2023-11-02 10:09:44 +0100

    --mongoose schema

---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  pic: {
+    type: String,
+    required: true,
+    default:
+      "https://res.cloudinary.com/dhlv2hnt4/image/upload/v1698836417/WhatsApp_Image_2023-10-25_at_11.14.35_AM_jecygv.jpg",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+  
