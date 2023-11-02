be5094b2ea3da11a63d93b12c143350945ddb9ab
leinadDebug <akujuaobidaniel@gmail.com>
2023-11-02 10:10:51 +0100

    --Array of object ; backend api

---
+++ backend/data/data.js
@@ -0,0 +1,141 @@
+const chats = [
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "John Doe",
+          email: "JohnDoe@gmail.com"
+        },
+        {
+          name: "Mustafa",
+          email: "Mustafa@gmail.com"
+        }
+      ],
+      _id: "617a077e18c25468bc7c4dd4",
+      chatName: "John Doe"
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Alice Smith",
+          email: "AliceSmith@gmail.com"
+        },
+        {
+          name: "Bob Johnson",
+          email: "BobJohnson@gmail.com"
+        }
+      ],
+      _id: "617a07b818c25468bc7c4dd5",
+      chatName: "Fun Group Chat"
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Sarah Adams",
+          email: "SarahAdams@gmail.com"
+        },
+        {
+          name: "Michael Brown",
+          email: "MichaelBrown@gmail.com"
+        }
+      ],
+      _id: "617a081418c25468bc7c4dd6",
+      chatName: "Sarah & Michael"
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Emily Davis",
+          email: "EmilyDavis@gmail.com" 
+        },
+        {
+          name: "David Wilson",
+          email: "DavidWilson@gmail.com"
+        }
+      ],
+      _id: "617a084d18c25468bc7c4dd7",
+      chatName: "Work Team"
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Linda Martinez",
+          email: "LindaMartinez@gmail.com"
+        },
+        {
+          name: "Thomas Turner",
+          email: "ThomasTurner@gmail.com"
+        }
+      ],
+      _id: "617a08a018c25468bc7c4dd8",
+      chatName: "Coffee Break"
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Sophia Harris",
+          email: "SophiaHarris@gmail.com"
+        },
+        {
+          name: "Daniel Clark",
+          email: "DanielClark@gmail.com"
+        }
+      ],
+      _id: "617a08e418c25468bc7c4dd9",
+      chatName: "Development Team"
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Olivia Baker",
+          email: "OliviaBaker@gmail.com"
+        },
+        {
+          name: "Matthew White",
+          email: "MatthewWhite@gmail.com"
+        }
+      ],
+      _id: "617a092718c25468bc7c4dda",
+      chatName: "Study Buddies"
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "Ava Lewis",
+          email: "AvaLewis@gmail.com"
+        },
+        {
+          name: "James Green",
+          email: "JamesGreen@gmail.com"
+        }
+      ],
+      _id: "617a095c18c25468bc7c4ddb",
+      chatName: "Project Collaboration"
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Chloe Walker",
+          email: "ChloeWalker@gmail.com"
+        },
+        {
+          name: "William Taylor",
+          email: "WilliamTaylor@gmail.com"
+        }
+      ],
+      _id: "617a09a318c25468bc7c4ddc",
+      chatName: "Gaming Partners"
+    }
+  ];
+  
+  module.exports={chats}
+
+  
