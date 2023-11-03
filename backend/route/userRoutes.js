8492c98cce2b166e1d93ed673f232a81c17b8f65
leinadDebug <akujuaobidaniel@gmail.com>
2023-11-03 10:54:40 +0100

    protect allUser with token

--- backend/route/userRoutes.js
+++ backend/route/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const {registerUser, authUser, allUsers} = require('../controllers/Usercontrollers')
+const { protect } = require('../middleware/authmiddleware')
 
 
 const router = express.Router()
 
-router.route('/').post(registerUser).get(allUsers)
+router.route('/').post(registerUser).get(protect, allUsers)
 router.post('/login', authUser)
 
 module.exports = router;
